71fa71e1dafbcfe5a038a21b73f2eb3927c89081
Allan Rios <73731571+AllanGRios@users.noreply.github.com>
2026-08-02 00:53:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 thermal_conductivity = 16.2  # W/(m K)
 Cp_ss = 500  # J/(kg K)
 Cp_water = 4184  # J/(kg K)
-density_ss = 8000  # kg/m3
+density_ss = 7930  # kg/m3
 density_water = 1000  # kg/m3
-emissivity_ss304 = 0.59
+emissivity_ss304 = 0.8
 emissivity_water = 0.963
-heat_transfer_coef = 8  # W/(m2 k)
-T_env_C = 20  # Celcius
+heat_transfer_coef = 25  # W/(m2 k)
+T_env_C = 23  # Celcius
 T_env_K = T_env_C + 273.15  # Kelvin
 stephan_boltzman_coeff = 5.67e-8  # W/(m2 K4)
 A_side = 0.0806  # m2
 A_top = 0.046  # m2
 
 T_start_C = 72.0
-simulation_minutes = 60
+simulation_minutes = 90
 dt = 1
 total_steps = simulation_minutes * 60
 
@@ -57,14 +57,19 @@ for step in range(total_steps):
     temperature_history.append(current_T_C)
     fc_loss_history.append(q_fc)
     rad_loss_history.append(q_rad)
-
+    if 61.99 < temperature_history[step] < 62:
+        SEC = (step/60 - step//60)*60
+        MIN = step/60 - (step/60 - step//60)
+    elif 61.98 < temperature_history[step] < 62:
+        SEC = (step / 60 - step // 60) * 60
+        MIN = step / 60 - (step / 60 - step // 60)
 plt.figure(figsize=(12, 5))
 
 # Temperature Decay Curve
 plt.subplot(1, 2, 1)
 plt.plot(time_axis, temperature_history, color="red", label="Uninsulated Pot")
-plt.axhline(62, color="blue", linestyle="--", label="Minimum Mash Temp (62°C)")
-plt.title("Mash Temperature Profile over 1 Hr")
+plt.axhline(62, color="blue", linestyle="--", label=f"Minimum Wort Temp (62°C)\nTime to minimum {int(MIN)} min {int(SEC)} s")
+plt.title("Wort Temperature Profile over 90 mins")
 plt.xlabel("Time (Minutes)")
 plt.ylabel("Temperature (°C)")
 plt.grid(True)
